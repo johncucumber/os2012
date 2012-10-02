681588680771f2d129781c38e9d581eea72f5783
pashok <pash-ok93@mail.ru>
2012-10-02 21:53:24 +0400

    blaablabalbalbalbal 2

--- OS_1st.c
+++ OS_1st.c
@@ -1,36 +1,64 @@
 #include <stdio.h>
 #include <math.h>
 #include <unistd.h>
+#include <pthread.h>
+ 
+void *qs(void *ptr);
+struct Params //передаю значения в метод QS
+{
+	int* s_arr;
+	int first;
+	int last;
+};
  
 int main(void)
-{	int i;
-	int a[10]={25,1,12,8,9,5,44,10,12,19};
-	for(i=0;i<=9;i++) printf("%d ", a[i]);printf("\n");
+{	
+	pthread_t thread;
+	int i;
 	
-	qs(&a,0,9);
-	for(i=0;i<=9;i++) printf("%d ", a[i]);
+	int a[10]={25,1,12,8,9,5,44,10,2,19};//создал массив
+	for(i=0;i<=9;i++) printf("%d ", a[i]);printf("\n");	
+	
+	struct Params p = { &a, 0, 9 };
+	pthread_create(&thread, NULL, qs, (void*) &p);//создал поток с методом QS
+	pthread_join(thread, NULL );
+	
+//	qs(&a,0,9);
+	for(i=0;i<=9;i++) printf("%d ", a[i]);//Вывод масссива
 	return 0;
 }
 
-void qs(int* s_arr, int first, int last)
+
+
+void *qs(void *ptr) 
 {
-    int i = first, j = last, x = s_arr[(first + last) / 2];
+	struct Params *p;
+	p = (struct Params *) ptr;//читаем переданные параметры
+
+    int i = p->first, j = p->last, x = p->s_arr[(p->first + p->last) / 2];
  
     do {
-        while (s_arr[i] < x) i++;
-        while (s_arr[j] > x) j--;
+        while (p->s_arr[i] < x) i++;
+        while (p->s_arr[j] > x) j--;
  
         if(i <= j) {
-            if (i < j) swap(&s_arr[i], &s_arr[j]);
+            if (i < j) swap(&p->s_arr[i], &p->s_arr[j]);//раскидываем в разные стороны от осевого элемента
             i++;
             j--;
         }
     } while (i <= j);
  
-    if (i < last)
-        qs(s_arr, i, last);
-    if (first < j)
-        qs(s_arr, first,j);
+		//повторяем рекурсивно
+    if (i < p->last)
+    {
+		struct Params p1 = { p->s_arr, i, p->last };
+        qs(&p1);
+    }
+    if (p->first < j)
+    {
+		struct Params p1 = { p->s_arr, p->first,j };
+        qs(&p1);
+	}
 }
 
 void swap(int *a,int *b)

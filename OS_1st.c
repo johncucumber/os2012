f550961ea83211cba10b7e7782eb36536bfb077b
pashok <pash-ok93@mail.ru>
2012-10-02 22:43:37 +0400

    3rd

--- OS_1st.c
+++ OS_1st.c
@@ -4,6 +4,7 @@
 #include <pthread.h>
  
 void *qs(void *ptr);
+void swap(int *a,int *b);
 struct Params //передаю значения в метод QS
 {
 	int* s_arr;
@@ -13,22 +14,39 @@ struct Params //передаю значения в метод QS
  
 int main(void)
 {	
+	int length=50;
+	int countThr=3;
+	int a[length];
+	
 	pthread_t thread;
 	int i;
 	
-	int a[10]={25,1,12,8,9,5,44,10,2,19};//создал массив
-	for(i=0;i<=9;i++) printf("%d ", a[i]);printf("\n");	
+	//создал массив
+	//int a[10]={25,1,12,8,9,5,44,10,2,19};
+	for(i;i<=length;i++) a[i]=rand()%100;
+	for(i=0;i<=length;i++) printf("%d ", a[i]);printf("\n");	
 	
-	struct Params p = { &a, 0, 9 };
+	/*struct Params p = { &a, 0, length };
 	pthread_create(&thread, NULL, qs, (void*) &p);//создал поток с методом QS
-	pthread_join(thread, NULL );
+	pthread_join(thread, NULL );*/
+	
+	int s=length/countThr; //длина части массива обрабатываемого 1  потоком
+	for (i = 0; i < countThr; i++) 
+	{			
+		struct Params p = { &a, i*s, i*s+s};
+		if (i == (countThr - 1))
+			p.last=length;
+		//Создание потоков
+		pthread_create(&thread, NULL, qs, (void*) &p);
+		pthread_join(thread, NULL );
+	}
+	
 	
 //	qs(&a,0,9);
-	for(i=0;i<=9;i++) printf("%d ", a[i]);//Вывод масссива
+	printf("\n");
+	for(i=0;i<=length;i++) printf("%d ", a[i]);//Вывод масссива
 	return 0;
 }
-
-
 
 void *qs(void *ptr) 
 {

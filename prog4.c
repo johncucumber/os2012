9d2890d850eb586cfe290f2487ed7b62da58d687
Alex Shevlyakov <toumutn@mail.ru>
2012-10-06 20:24:37 +0400

    New edit

--- prog4.c
+++ prog4.c
@@ -14,13 +14,38 @@ typedef struct mymsgbuf
   int sum;
 } mess_t;
 
+void saya(int semid, int qid, struct sembuf lock_res, struct sembuf rel_res, mess_t *buf, int length, char type)
+{
+	while (buf->num<13)
+	{
+	  sleep(rand()%3);       
+	  if (semop(semid, &lock_res, 1) != -1)
+	  {
+	    if (msgrcv(qid, buf, length, 1, 0)!=-1)
+	    {
+	      if (type=='s') 
+	      	printf("SON %d - NUM %d: SUM %d\n", getpid(),buf->num, buf->sum);
+	      else 
+		printf("FATHER - NUM %d: SUM %d\n", buf->num, buf->sum); 
+	      if (buf->num<13)
+	      {
+	       	buf->num++;
+		buf->sum*=buf->num;
+	      }
+              msgsnd(qid, buf, length, 0);
+    	      semop(semid, &rel_res, 1); 	     		    
+	    }
+	  } 
+	}
+}
+
 int main()
 {
   int qid;
-  key_t msgkey,key;
+  key_t msgkey;
   pid_t pid;
 
-  mess_t buf;
+  mess_t buf,buf2;
 
   int length;
 
@@ -45,6 +70,7 @@ int main()
   buf.num=1;
   buf.sum=1;        
   buf.mtype=1;
+  
   msgsnd(qid, &buf, length, 0);
 
   for (i = 0; i < 4; i++)
@@ -52,41 +78,12 @@ int main()
     pid = fork();
     if(!pid)
     {
-      while (buf.num<13)
-      {
-        sleep(rand()%3);
-        if (semop(semid, &lock_res, 1) != -1)
-        {
-        
-          if (msgrcv(qid, &buf, length, 1, 0)!=-1)
-          {
-            printf("SON %d - NUM %d: SUM %d\n", getpid(),buf.num, buf.sum);
-            buf.num++;
-            buf.sum*=buf.num;
-            msgsnd(qid, &buf, length, 0);
-          }
-          semop(semid, &rel_res, 1);
-        }
-      }    
+      saya(semid,qid,lock_res,rel_res,&buf,length,'s');
       exit(0);
     }
   }
    
-  while (buf.num<13)
-  {   
-     sleep(rand()%2);
-     if (semop(semid, &lock_res, 1) != -1)
-     {        
-       if (msgrcv(qid, &buf, length, 1, 0)!=-1)
-       {      
-         printf("FATHER - NUM %d: SUM %d\n", buf.num, buf.sum);
-         buf.num++;
-         buf.sum*=buf.num;
-         msgsnd(qid, &buf, length, 0);
-       }
-     semop(semid, &rel_res, 1);
-     }
-  }
+  saya(semid,qid,lock_res,rel_res,&buf,length,' ');  
 
   msgctl(qid, IPC_RMID, 0);
   semctl(semid, 0, IPC_RMID);

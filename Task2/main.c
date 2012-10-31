7662cad1d19bd0208294f74840d06a145ad895c2
Dmitriy <dimko@russia.ru>
2012-10-31 20:28:48 +0400

    Task2. The beginning

---
+++ Task2/main.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <pthread.h> 
+#include <semaphore.h>
+
+int THC; 
+
+int main(int argc, char **argv) {
+	
+	/* Задаем кооличество потоков*/
+	if (argc == 2) {
+		THC = atoi(argv[1]);
+	} else
+	printf("Please set thread count\n");
+	return 0;
+}

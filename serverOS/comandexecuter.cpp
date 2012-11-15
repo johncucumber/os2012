8c5a561f93d298b075a7d32c89cae4e804fcc319
kirill klimov <ra22341@ya.ru>
2012-11-15 13:08:13 +0400

    dishoored

--- serverOS/comandexecuter.cpp
+++ serverOS/comandexecuter.cpp
@@ -1,6 +1,7 @@
 #include "comandexecuter.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void ComandExecuter::PrintVec(std::vector<std::string> vec)
 {
@@ -13,11 +14,12 @@ void ComandExecuter::PrintVec(std::vector<std::string> vec)
 std::vector<std::string> ComandExecuter::Execute(std::string cmd)
 {
     std::vector<std::string> res;
-    FILE* ptr = popen(cmd.c_str(), "r");
+    char buf[512];
+    strcpy(buf, cmd.c_str());//ta-dadam!
+    FILE* ptr = popen(buf, "r");
 
     if (ptr != NULL)
-    {
-        char buf[BUFSIZ];
+    {   
         while (fgets(buf, BUFSIZ, ptr) != NULL)
             res.push_back(buf);
             //(void) printf("~>%s", buf);

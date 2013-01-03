0fdbc3ed9beb7c5aca75f4ffb6ca6e4912edfb75
kirill klimov <ra22341@ya.ru>
2013-01-03 18:40:18 +0400

    ...

--- fuse/fswork.c
+++ fuse/fswork.c
@@ -7,16 +7,29 @@ void initFileSystem(void)
 {
     struct filestruct nodes[MAX_NODES];
     FILE *output;
-    int i;
     if((output=fopen(FS_FILE_PATH, "a+"))==0)
     {
         puts ("Can't open fs's file.");
         exit(-1);
     }
     fwrite(nodes, 1, sizeof(nodes), output);
-    /*for (i = 0; i < MAX_NODES; i++)
-    {
-        fwrite(output, nodes[i], sizeof(struct filestruct));
-    }*/
     fclose(output);
 }
+
+struct filestruct *getNodes()
+{
+    struct filestruct *nodes = malloc(sizeof(struct filestruct)*MAX_NODES);
+    FILE *input;
+    int i;
+    if((input=fopen(FS_FILE_PATH, "a+"))==0)
+    {
+        puts ("Can't open fs's file.");
+        exit(-1);
+    }
+
+
+    fclose(input);
+
+    return nodes;
+}
+
--- fuse/fswork.h
+++ fuse/fswork.h
@@ -10,3 +10,4 @@ struct filestruct
 };
 
 void initFileSystem();
+struct filestruct *getNodes();

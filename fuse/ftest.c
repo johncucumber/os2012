8b98f4c854bfb4f0df083350c67d4cf1ce18e356
kirill klimov <ra22341@ya.ru>
2013-01-04 17:37:41 +0400

    add ftest

---
+++ fuse/ftest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "log.h"
+#include "fswork.h"
+
+int main(int argc, char **argv)
+{
+    printf("Start....\n");
+
+    printf("Finish\n");
+    return 0;
+}

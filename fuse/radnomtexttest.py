d9b3c3b946527afb95f3f57aba45c2f3b55e8bba
kirill klimov <ra22341@ya.ru>
2013-01-07 17:54:44 +0400

    changes in test

--- fuse/radnomtexttest.py
+++ fuse/radnomtexttest.py
@@ -1,25 +1,31 @@
 import subprocess as su
+#import sys
 from random import random
 
 print 'Start test'
 
 su.call('fusermount -u td ', shell=True)
 su.call('make', shell=True)
-su.call('./createfs', shell=True)
+#su.call('./createfs', shell=True)
 su.call('./cfs td', shell=True)
 
 res = '' 
-fname = 'td/ptest'
+#fname = sys.argv[1]
+dirname = 'td/testdir'
+su.call('mkdir ' + dirname, shell=True)
+fname = 'td/testdir/test'
+su.call('rm ' + fname, shell=True)
 f = open(fname, 'w')
-for i in xrange(100):
+for i in xrange(100000):
     s = ''
-    for j in xrange(int(random()*100)):
-        s += chr(60 + int(random()*10))
-    print i, ':', s
+    for j in xrange(int(random()*200)):
+        s += chr(60 + int(random()*30))
+    #print i, ':', s
     s += '\n'
     res += s
 f.write(res)
 f.close
+print 'data len', len(res)
 su.call('sleep 3', shell=True)
 f = open(fname, 'r')
 s = f.read()
@@ -42,3 +48,7 @@ if s != res:
 print 'data valid'
 f.close()
 print 'Test passed'
+print 'rm', fname
+su.call('rm ' + fname, shell=True)
+print 'rmdir', dirname
+su.call('rmdir ' + dirname, shell=True)

8c4589e014fd40e503f72ba4cc9551e12ffb7b68
kirill klimov <ra22341@ya.ru>
2013-01-09 15:24:07 +0400

    more tests

--- fuse/test.py
+++ fuse/test.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
         print(' >Making subdir sucsessfull')
 
     print('mv test')
-    new_test_file_path = '%stestdir/d2/f2' % test_dir
+    new_test_file_path = '%stestdir/d2/mvfile' % test_dir
     su.call('mv %s %s' % (test_file_path, new_test_file_path), shell=True)
     if os.path.exists(test_file_path):
         print(' >mv file failed: old file exists')
@@ -85,6 +85,61 @@ if __name__ == '__main__':
                 exit()
             else:
                 print(' >mv file sucsessfull. Data valid')
-     
+
+    print('Unmount test')
+    print('Waiting for unmount')
+    waiting()
+    umount()
+    mount()
+    if os.path.exists(test_file_path):
+        print(' >unmount  failed: old file exists')
+        test_finish()
+        exit()
+    else:
+        if not os.path.exists(new_test_file_path):
+            print(' >unmount failed: file not exists')
+            test_finish()
+            exit()
+        else:
+            if not filecmp.cmp('fswork.c', new_test_file_path):
+                print(' >unmount failed: bad data')
+                test_finish()
+                exit()
+            else:
+                print(' >unmount sucsessfull. Data valid')
+
+
+    print('many files cp test')
+    for i in xrange(40):
+        tmp_test_file_path = new_test_file_path + str(i)
+        su.call('cp %s %s' % (new_test_file_path, tmp_test_file_path), shell=True)
+        if not os.path.exists(tmp_test_file_path):
+            print(' >cp file failed: file not exists')
+            test_finish()
+            exit()
+        else:
+            if not filecmp.cmp(new_test_file_path, tmp_test_file_path):
+                print(' >cp file failed: bad data')
+                test_finish()
+                exit()
+    print(' >many cp test passed!') 
+    print('Unmount test2')
+    print('Waiting for unmount')
+    waiting()
+    umount()
+    mount()
+    for i in xrange(40):
+        tmp_test_file_path = new_test_file_path + str(i)
+        if not os.path.exists(tmp_test_file_path):
+            print(' >cp file failed: file not exists')
+            test_finish()
+            exit()
+        else:
+            if not filecmp.cmp(new_test_file_path, tmp_test_file_path):
+                print(' >cp file failed: bad data')
+                test_finish()
+                exit()
+    print(' >Unmount 2 test passed!') 
+    print('\nAll tests passed!')
     test_finish()
 

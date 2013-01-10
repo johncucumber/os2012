a98d3098dc34e77fb0e2b2acadf957bf149c5aa9
kirill klimov <ra22341@ya.ru>
2013-01-10 14:13:28 +0400

    Delete the past, the fear\n Delete the past, every step is pure\n Delete the past from here, forward\n Delete the past, every step is pure!

--- fuse/checkfiles.sh
+++
@@ -1,5 +0,0 @@
-  if ((diff $1 $2) &> /dev/null) && ((diff $2 $1) &> /dev/null); then
-    echo "OK"
-  else
-    echo "FAIL"
-  fi
--- fuse/log.h
+++
@@ -1,3 +0,0 @@
-#define LOG_PATH "/home/kirill/coding/MyPy/os2012/fuse/cfs_support/log.txt"
-
-void addLog(const char *str);
